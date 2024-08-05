fe4fbb6faf980775244d19b6848a288d419e370f
Doocheol <encjf2070@gmail.com>
2024-08-05 21:03:21 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.03 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,17 +1,17 @@
 class Solution {
     public int[] solution(int n) {
         
-        int[] arr;
-        if (n % 2 == 0) {
-            arr = new int[n / 2];
-        } else {
-            arr = new int [(n+1)/2];
+        int[] arr = new int[(int) (n-1)/2];
+        
+        int num = 1;
+        for (int i = 0; i < (n-1)/2; i++) {
+            num++;
+            if (num % 2 != 0) {
+                arr[i] = num++;
+            }
         }
         
-        int idx = 0;
-        for (int i = 1; i <= arr.length; i++) {
-            arr[idx++] = i * 2 - 1;
-        }
+        
         
         return arr;
     }
