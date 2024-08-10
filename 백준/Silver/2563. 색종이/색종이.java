7d9fc60d10c7a97d09c176b604c89ba90239a0f9
Doocheol <encjf2070@gmail.com>
2024-08-10 22:51:57 +0900

    [Silver V] Title: 색종이, Time: 100 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,45 +1,53 @@
+
 import java.io.*;
+import java.util.*;
 
 public class Main {
-
 	public static void main(String[] args) throws Exception {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder(100);
+		StringBuilder sb = new StringBuilder();
+
+		final int SIZE = 101;
 		
-		int n = Integer.parseInt(br.readLine().trim());
 		
-		boolean[][] check = new boolean[100][100];
+		// 중복 체크를 위한 boolean 배열 선언
+		boolean[][] board = new boolean[SIZE][SIZE];
+		int N = Integer.parseInt(br.readLine().trim());
 		
-		int x = 0; // x좌표
-		int y = 0; // y좌표 
-		
-		for (int i = 0; i < n; i++) {
-			String[] str = br.readLine().split(" ");
-			x = Integer.parseInt(str[0]);
-			y = Integer.parseInt(str[1]);
+		// 색종이 갯수만큼 계산하기
+		for (int index = 1; index <= N; index++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
 			
 			
-			for (int r = x; r < x + 10; r++) {
-				for (int c = y; c < y + 10; c++) {
-					check[r][c] = true;
+			// 색종이의 크기 10 x 10
+			for (int i = x; i < x + 10; i++) {
+				for (int j = y; j < y + 10; j++) {
+					board[i][j] = true;
 				}
 			}
 
 		}
-		int Ans = 0;
-		for (int r = 0; r < 100; r++) {
-			for (int c = 0; c < 100; c++) {
-				if (check[r][c]) {
-					Ans++;
-				}
+		
+		// 해당 인덱스에 true 표시가 되었다면 sum++
+		int sum = 0;
+		int[] area = new int[N + 1];
+		for (int row = 0; row < SIZE; row++) {
+			for (int column = 0; column < SIZE; column++) {
+				if (board[row][column])
+					sum++;
 			}
 		}
-		sb.append(Ans);
+
+		sb.append(sum);
 		bw.write(sb.toString());
 		bw.flush();
 		bw.close();
 		br.close();
-	}
 
+	}
 }
