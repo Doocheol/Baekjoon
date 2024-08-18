b9da311d1ac54715d349b92218575c844c591b35
Doocheol <encjf2070@gmail.com>
2024-08-19 00:03:07 +0900

    [Silver II] Title: 창고 다각형, Time: 112 ms, Memory: 14492 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.java
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.java
@@ -12,59 +12,46 @@ public class Main {
 		
 		int N = Integer.parseInt(br.readLine().trim());
 		
-		int[][] building = new int[N][2];
+		int[] heights = new int[1001];
 		
-		for (int row = 0; row < N; row++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			building[row][0] = Integer.parseInt(st.nextToken());  // x 좌표
-			building[row][1] = Integer.parseInt(st.nextToken());  // 높이
-		}
+		int max = Integer.MIN_VALUE;
+		int maxIdx = Integer.MIN_VALUE;
+		int sum = 0;
 		
-		// x 좌표 기준으로 오름차순 정렬
-		Arrays.sort(building, Comparator.comparingInt(o -> o[0]));
-		
-		// 최대 높이 찾기
-		int maxIdx = 0;
-		int maxHeight = 0;
 		for (int i = 0; i < N; i++) {
-			if (building[i][1] > maxHeight) {
-				maxHeight = building[i][1];
-				maxIdx = i;
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int position = Integer.parseInt(st.nextToken());
+			int height = Integer.parseInt(st.nextToken());
+			heights[position] = height;
+			if (height > max) {
+				max = height;
+				maxIdx = position;
 			}
 		}
 		
-		// 좌측에서 최대 높이까지 면적 구하기
-		int area = 0;
-		int leftHeight = building[0][1];
-		for (int i = 1; i <= maxIdx; i++) {
-			if (building[i][1] > leftHeight) {
-				area += leftHeight * (building[i][0] - building[i-1][0]);
-				leftHeight = building[i][1];
-			} else {
-				area += leftHeight * (building[i][0] - building[i-1][0]);
+		int left = 0;
+		for (int i = 0; i <= maxIdx; i++) {
+			if (heights[i] > left) {
+				left = heights[i];
 			}
+			sum += left;
 		}
 		
-		// 우측에서 최대 높이까지 면적 구하기
-		int rightHeight = building[N-1][1];
-		for (int i = N-2; i >= maxIdx; i--) {
-			if (building[i][1] > rightHeight) {
-				area += rightHeight * (building[i+1][0] - building[i][0]);
-				rightHeight = building[i][1];
-			} else {
-				area += rightHeight * (building[i+1][0] - building[i][0]);
+		int right = 0;
+		for (int i = heights.length - 1; i > maxIdx; i--) {
+			if (heights[i] > right) {
+				right = heights[i];
 			}
+			sum += right;
 		}
 		
-		// 최대 높이 부분 더하기
-		area += maxHeight;
-		
-		// 최종 면적 출력
-		sb.append(area);
+		sb.append(sum);
 		bw.write(sb.toString());
 		bw.flush();
 		bw.close();
 		br.close();
+		
+		
 	}
 
 }
