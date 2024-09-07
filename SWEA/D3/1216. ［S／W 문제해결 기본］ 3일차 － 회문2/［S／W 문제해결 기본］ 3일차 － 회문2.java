483da495ec1e36cfce43bc89ca21c1dbcaf3457c
Doocheol <encjf2070@gmail.com>
2024-09-07 22:35:51 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문2, Time: 199 ms, Memory: 43,984 KB -BaekjoonHub

---
+++ SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.java
@@ -0,0 +1,87 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Solution {
+
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		
+		for (int tc = 1; tc<=10; tc++) {
+			
+			int N = Integer.parseInt(br.readLine());
+			char[][] board = new char[100][100];
+			for (int row = 0; row < 100; row++) {
+				String line = br.readLine();
+				for (int column = 0; column < 100; column++) {
+					board[row] = line.toCharArray();
+				}
+			}
+			
+			int maxLength = 1;
+			
+			//행 탐색
+			for (int row = 0; row < 100; row++) {
+				for (int start = 0; start < N; start++) {
+					for (int end = 99; end > start; end--) {
+						if (checkRow(board, start, end, row)) {
+							maxLength = Math.max(maxLength, end - start + 1);
+						}
+					}
+					
+				}
+			}
+			
+			for (int column = 0; column < 100; column++) {
+				for (int start = 0; start < 100; start++) {
+					for (int end = 99; end > start; end--) {
+						if (checkColumn(board, start, end, column)) {
+							maxLength = Math.max(maxLength, end - start + 1);
+						}
+					}
+				}
+			}
+			sb.append("#").append(N).append(" ").append(maxLength).append("\n");
+			
+		} // End tc
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	} // End main
+
+	private static boolean checkRow(char[][] arr, int start, int end, int row) {
+
+		while (start < end) {
+			
+			if (arr[row][start] != arr[row][end]) {
+				return false;
+			}
+			
+			start++;
+			end--;
+		}
+		
+		return false;
+	}
+	
+	private static boolean checkColumn(char[][] arr, int start, int end, int column) {
+
+		while (start < end) {
+			
+			if (arr[start][column] != arr[end][column]) {
+				return false;
+			}
+			
+			start++;
+			end--;
+		}
+		
+		return false;
+	}
+	
+} // End class
