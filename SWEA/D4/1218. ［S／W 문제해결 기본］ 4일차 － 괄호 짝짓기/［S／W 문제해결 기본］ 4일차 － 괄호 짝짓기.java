e065721cda9d0e051fdb0fb19bf855ac016e7ca7
Doocheol <encjf2070@gmail.com>
2024-09-08 15:27:05 +0900

    [D4] Title: [S/W 문제해결 기본] 4일차 - 괄호 짝짓기, Time: 115 ms, Memory: 18,380 KB -BaekjoonHub

--- SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.java
+++ SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.java
@@ -1,63 +1,54 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Solution {
 
 	public static void main(String[] args) throws Exception {
 		
-//		System.setIn(new FileInputStream("src/김두철/A020.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
 		
-		//테스트케이스 10개
-		for (int tc = 1; tc <= 10; tc++) {
-			int T = Integer.parseInt(br.readLine().trim());
-			String line = br.readLine().trim();
-			Stack<Character> stack = new Stack<>();
+		Deque<Character> stack = new ArrayDeque<>();
+		
+		for (int tc = 1; tc<=10; tc++) {
+			int CHAR_LENGTH = Integer.parseInt(br.readLine());
+			String line = br.readLine();
 			
 			int answer = 0;
-			for (int i = 0; i < line.length(); i++) {
+			for (int i = 0; i < CHAR_LENGTH; i++) {
+				char ch = line.charAt(i);
 				
-				// 여는 괄호를 만나면 스택에 넣는다.
-				if (line.charAt(i) == '(' ||
-					line.charAt(i) == '{' ||
-					line.charAt(i) == '[' ||
-					line.charAt(i) == '<') {
-					stack.push(line.charAt(i));
-				// 닫는 괄호를 만나면
+				if (ch == '(' || ch == '{' || ch == '<' || ch == '[') {
+					stack.offerLast(ch);
 				} else {
-					// 스택이 비어 있으면 바로 유효하지 않기 때문에 0을 보관하고 break
 					if (stack.isEmpty()) {
 						answer = 0;
 						break;
-						
-					}
-					// 스택을 pop 했을 때, 쌍을 이루지 못하면 0을 보관하고 break
-					if ((line.charAt(i) == ')' && stack.pop() != '(') ||
-						(line.charAt(i) == '}' && stack.pop() != '{') ||
-						(line.charAt(i) == ']' && stack.pop() != '[') ||
-						(line.charAt(i) == '>' && stack.pop() != '<')) {
-							answer = 0;
-							break;
+					} else if ((ch == ')' && stack.pollLast() != '(') ||
+							(ch=='}' && stack.pollLast() != '{') ||
+							(ch =='>' && stack.pollLast() != '<') ||
+							(ch == ']' && stack.pollLast() != '[')) {
+						answer = 0;
+						break;
 					} else {
 						answer = 1;
 					}
 					
 				}
-				
-				// 스택이 남아 있으면 불안정한 상태 answer 0
-				if (!stack.isEmpty()) {
-					answer = 0;
-				} 
 			}
 			sb.append("#").append(tc).append(" ").append(answer).append("\n");
+			
 		}
 		bw.write(sb.toString());
 		bw.flush();
 		bw.close();
 		br.close();
+		
 	}
+	
 }
-
-
