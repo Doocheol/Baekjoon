7501f1b616e084a92593ebb3299dd9b51c563ea9
Doocheol <encjf2070@gmail.com>
2024-12-10 16:30:37 +0900

    [level 2] Title: 올바른 괄호, Time: 16.44 ms, Memory: 55.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -10,17 +10,19 @@ class Solution {
         for (char c : ca) {
             if (c == '(') stack.push(c);
             else {
-                if (stack.isEmpty() || stack.pop() == c) return false;
+                if (stack.isEmpty() || stack.pop() == c) {
+                    return false;
+                }
             }
         }
         
-        return stack.isEmpty();
         
+        return stack.isEmpty();
 //         boolean answer = true;
 
 //         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
 //         System.out.println("Hello Java");
 
-//         return answer;
+        // return answer;
     }
 }
