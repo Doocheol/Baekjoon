f9820080bcc453a0152214154e59f423a4be93c8
Doocheol <encjf2070@gmail.com>
2024-12-11 18:46:21 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.16 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -1,47 +1,44 @@
 import java.util.*;
 import java.io.*;
 
+
 class Solution {
     public int solution(int[][] board, int[] moves) {
-        int answer = 0;
         
         int n = board.length;
-        int[][] clone = board.clone();
-        // for (int i = 0; i < board.length; i++) {
-            // clone[i] = System.arrayCopy(board[i]);
-        // }
         
-        
-        Deque<Integer> stack = new LinkedList<>();
-        
-        
-        for (int i = 0; i < moves.length; i++) {
-            
-            int columnIndex = moves[i] - 1;
-            
-            for (int j = 0; j < n; j++) {
-                
-                int value = clone[j][columnIndex];
-                
-                if (value != 0) {
-                    
-                    clone[j][columnIndex] = 0;
-
-                    if (!stack.isEmpty() && stack.peek() == value) {
-                        stack.pop();
-                        answer += 2;
-                    } else {
-                        stack.push(value);
-                    }
-                    
-                    break;
-                }
-                
-                
-            }
-            
+        int[][] boardCopy = new int[n][];
+        for (int i = 0; i < n; i++) {
+            boardCopy[i] = Arrays.copyOf(board[i], board[i].length);
         }
         
+        ArrayDeque<Integer>[] lanes = new ArrayDeque[n];
+        for (int i = 0; i < lanes.length; i++) {
+            lanes[i] = new ArrayDeque<>();
+        }
+        
+        for (int i = board.length - 1; i >= 0; i--) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] > 0) {
+                    lanes[j].push(board[i][j]);
+                }
+            }
+        }
+        
+        ArrayDeque<Integer> bucket = new ArrayDeque<>();
+        int answer = 0;
+        
+        for (int move : moves) {
+            if (!lanes[move - 1].isEmpty()) {
+                int doll = lanes[move - 1].pop();
+                if (!bucket.isEmpty() && bucket.peek() == doll) {
+                    bucket.pop();
+                    answer += 2;
+                } else {
+                    bucket.push(doll);
+                }
+            }
+        }
         
         return answer;
     }
