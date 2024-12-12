c7e2e3c56a932ea4cfe8e0b8efb2a44d93e76b59
Doocheol <encjf2070@gmail.com>
2024-12-12 16:06:47 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 76.91 ms, Memory: 97 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for (String name : completion) {
+            map.put(name, map.getOrDefault(name, 0) + 1);
+        }
+        
+        for (String name : participant) {
+            if (map.getOrDefault(name, 0) == 0) {
+                return name;
+            }
+            map.put(name, map.get(name) - 1);
+        }
+        return null;
+
+    }
+}
