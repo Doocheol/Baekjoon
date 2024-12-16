d87cf7b31eee8a918581717b39894dbe82a7c8c4
Doocheol <encjf2070@gmail.com>
2024-12-16 18:07:03 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 100.40 ms, Memory: 140 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -5,6 +5,7 @@ class Solution {
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
         
         HashMap<String, String> parent = new HashMap<>();
+        
         for (int i = 0; i < enroll.length; i++) {
             parent.put(enroll[i], referral[i]);
         }
@@ -19,15 +20,12 @@ class Solution {
                 money /= 10;
             }
         }
-        
+
         int[] answer = new int[enroll.length];
         for (int i = 0; i < enroll.length; i++) {
             answer[i] = total.getOrDefault(enroll[i], 0);
         }
-        
         return answer;
-    
-    
-    
+        
     }
 }
