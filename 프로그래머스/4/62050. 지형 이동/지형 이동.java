81837bf5a9a0b9c8220402f7b2f70aca6a7cd439
Doocheol <encjf2070@gmail.com>
2025-01-04 13:15:12 +0900

    [level 4] Title: 지형 이동, Time: 178.46 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/4/62050. 지형 이동/지형 이동.java
+++ 프로그래머스/4/62050. 지형 이동/지형 이동.java
@@ -2,8 +2,9 @@ import java.util.*;
 
 class Solution {
     
-    private static class Node{
+    private static class Node {
         int i, j, cost;
+        
         public Node(int i, int j, int cost) {
             this.i = i;
             this.j = j;
@@ -14,32 +15,30 @@ class Solution {
     public int solution(int[][] land, int height) {
         int answer = 0;
         int n = land.length;
-        boolean[][] visited = new boolean[n][n];
-        
-        
+
         int[] dr = {-1, 0, 1, 0};
         int[] dc = {0, -1, 0, 1};
+        boolean[][] visited = new boolean[n][n];
         
         PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1.cost, o2.cost));
         pq.add(new Node(0, 0, 0));
         
-        while (!pq.isEmpty()) {
+        while(!pq.isEmpty()) {
             Node now = pq.poll();
-            if (visited[now.i][now.j]) continue;
+            if(visited[now.i][now.j]) continue;
             visited[now.i][now.j] = true;
             answer += now.cost;
             
-            for (int d = 0; d < 4; d++) {
+            for (int d= 0; d<4; d++) {
                 int nr = now.i + dr[d];
                 int nc = now.j + dc[d];
                 
-                if(!(0<=nr && nr <n && 0<=nc && nc < n)) continue;
-                int tempCost = Math.abs(land[now.i][now.j] - land[nr][nc]);
+                if (!(0<=nr&&nr<n&&0<=nc&&nc<n)) continue;
+                int tempCost = Math.abs(land[nr][nc] - land[now.i][now.j]);
                 int newCost = tempCost > height ? tempCost : 0;
                 pq.add(new Node(nr, nc, newCost));
             }
         }
-        
         
         
         
