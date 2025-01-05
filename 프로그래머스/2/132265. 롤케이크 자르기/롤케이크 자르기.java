9bab0a567d080f07dea61bf8beaf2469f7d054fe
Doocheol <encjf2070@gmail.com>
2025-01-05 22:12:51 +0900

    [level 2] Title: 롤케이크 자르기, Time: 306.32 ms, Memory: 175 MB -BaekjoonHub

---
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] topping) {
+        int answer = 0;
+        HashMap<Integer, Integer> toppingMap = new HashMap<>();
+        for (int t : topping) {
+            toppingMap.put(t, toppingMap.getOrDefault(t, 0) + 1);
+        }
+        
+        HashSet<Integer> toppingSet = new HashSet<>();
+        for (int t : topping) {
+            toppingSet.add(t);
+            toppingMap.put(t, toppingMap.get(t) - 1);
+            
+            if (toppingMap.get(t) == 0) toppingMap.remove(t);
+            if (toppingMap.size() == toppingSet.size()) answer++;
+            
+            
+        }
+        return answer;
+    }
+}
