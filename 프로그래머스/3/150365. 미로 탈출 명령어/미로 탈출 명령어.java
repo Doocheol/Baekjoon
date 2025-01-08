a48077cf8b3858cac335a0d689b7cbb7dabecd4c
Doocheol <encjf2070@gmail.com>
2025-01-08 23:56:40 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 1.05 ms, Memory: 82.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -0,0 +1,51 @@
+class Solution {
+    public String solution(int n, int m, int x, int y, int r, int c, int k) {
+        
+        int currentX = x;
+        int currentY = y;
+        StringBuilder path = new StringBuilder();
+        char[] directions = {'d', 'l', 'r', 'u'};
+        int[] dx = {1, 0, 0, -1};
+        int[] dy = {0, -1, 1, 0};
+
+        
+        int initialDistance = Math.abs(x - r) + Math.abs(y - c);
+        if (initialDistance > k || (k-initialDistance) % 2 != 0) {
+            return "impossible";
+        }
+
+        
+        for (int step = 0; step < k; step++) {
+            boolean moved = false;
+            for (int d = 0; d < directions.length; d++) {
+                char dir = directions[d];
+                int nextX = currentX + dx[d];
+                int nextY = currentY + dy[d];
+                if (nextX < 1 || nextX > n || nextY < 1 || nextY > m) continue;
+                int distance = Math.abs(nextX - r) + Math.abs(nextY - c);
+                int nextRemaining = k - (step + 1);
+                
+                if (distance > nextRemaining) continue;
+                if ((nextRemaining - distance) % 2 != 0) continue;
+                
+                path.append(dir);
+                currentX = nextX;
+                currentY = nextY;
+                moved = true;
+                break;
+            }
+            
+            if (!moved) {
+                return "impossible";
+            }
+        }
+        if (currentX == r && currentY == c) {
+            return path.toString();
+        }
+        else {
+            return "impossible";
+        }
+                
+        
+    }
+}
